4078a4190781876c3b61515e4e2dfb55fcb01d49
Fabrizio Pietrucci <mail@fabrizio-pietrucci.it>
2025-12-03 13:00:28 +0100

    day 2 part 2

--- day2/day2.c
+++ day2/day2.c
@@ -21,11 +21,26 @@ static void write_int(size_t n, StringBuffer* out) {
     out->size += len;
 }
 
-static bool is_repeated(StringSlice ss) {
+static bool is_repeated_part1(StringSlice ss) {
     size_t mid = ss.size / 2;
     StringSlice first = ss_from(ss.data, mid);
     StringSlice second = ss_from(ss.data + mid, ss.size - mid);
     return ss_eq(first, second);
+}
+
+static bool is_repeated_part2(StringSlice ss) {
+    for(size_t seq_size = 1; seq_size <= ss.size / 2; seq_size++) {
+        StringSlice seq = ss_from(ss.data, seq_size);
+        for(size_t n = seq_size; n < ss.size; n += seq_size) {
+            StringSlice cmp = ss_from(ss.data + n, n + seq_size < ss.size ? seq_size : ss.size - n);
+            if(!ss_eq(seq, cmp)) {
+                goto continue_outer;
+            }
+        }
+        return true;
+    continue_outer:;
+    }
+    return false;
 }
 
 int main(int argc, char** argv) {
@@ -38,7 +53,8 @@ int main(int argc, char** argv) {
     StringBuffer input = {0};
     if(!read_file(input_file, &input)) return 1;
 
-    size_t sum = 0;
+    size_t part1 = 0;
+    size_t part2 = 0;
 
     StringSlice ss = sb_to_ss(input);
     StringBuffer str = {0};
@@ -50,11 +66,15 @@ int main(int argc, char** argv) {
         for(size_t n = first; n <= second; n++) {
             str.size = 0;
             write_int(n, &str);
-            if(is_repeated(sb_to_ss(str))) {
-                sum += n;
+            if(is_repeated_part1(sb_to_ss(str))) {
+                part1 += n;
+            }
+            if(is_repeated_part2(sb_to_ss(str))) {
+                part2 += n;
             }
         }
     }
 
-    printf("Result: %zu\n", sum);
+    printf("Part 1: %zu\n", part1);
+    printf("Part 2: %zu\n", part2);
 }
